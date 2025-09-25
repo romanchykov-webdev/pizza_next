5e23aaaaaae0a9183f92162d65adda5520ffd1c1
romanchykov-webdev <romanchykov.webdev@gmail.com>
2025-09-25 18:17:19 +0200

    next step

--- src/components/shared/choose-pizza-form.tsx
+++ src/components/shared/choose-pizza-form.tsx
@@ -1,98 +1,91 @@
-'use client';
+"use client";
 
-import { cn } from '@/lib/utils';
-import React from 'react';
-import { Button } from '../ui/button';
-import { Title } from './title';
-import { ProductImage } from '@/components/shared/product-image';
-import { GroupVariants } from '@/components/shared/group-variants';
-import { PizzaSize, PizzaType, pizzaTypes } from '@/constants/pizza';
-import { IngredientsList } from '@/components/shared/Ingredients-list';
-import { IProduct } from '../../../@types/prisma';
-import { getPizzaDetails } from '@/lib';
-import { usePizzaOptions } from '@/hooks/use-pizza-options';
-import { Ingredient, ProductItem } from '@prisma/client';
+import { GroupVariants } from "@/components/shared/group-variants";
+import { IngredientsList } from "@/components/shared/Ingredients-list";
+import { ProductImage } from "@/components/shared/product-image";
+import { PizzaSize, PizzaType, pizzaTypes } from "@/constants/pizza";
+import { cn } from "@/lib/utils";
+import React from "react";
+import { Button } from "../ui/button";
+import { Title } from "./title";
+
+import { usePizzaOptions } from "@/hooks/use-pizza-options";
+import { getPizzaDetails } from "@/lib";
+import { Ingredient, ProductItem } from "@prisma/client";
 
 interface Props {
-  imageUrl: string;
-  name: string;
-  className?: string;
-  ingredients: Ingredient[];
-  items?: ProductItem[];
-  onClickAdd?: VoidFunction;
+	imageUrl: string;
+	name: string;
+	className?: string;
+	ingredients: Ingredient[];
+	items?: ProductItem[];
+	onClickAdd?: VoidFunction;
 }
 
 export const ChoosePizzaForm: React.FC<Props> = ({
-  name,
-  items = [],
-  imageUrl,
-  ingredients,
-  onClickAdd,
-  className,
+	name,
+	items = [],
+	imageUrl,
+	ingredients,
+	onClickAdd,
+	className,
 }) => {
-  const { size, type, selectedIngredients, availableSizes, setType, setSize, addIngredient } =
-    usePizzaOptions(items);
+	const { size, type, selectedIngredients, availableSizes, setType, setSize, addIngredient } = usePizzaOptions(items);
 
-  const { textDetails, totalPrice } = getPizzaDetails(
-    type,
-    size,
-    items,
-    ingredients,
-    selectedIngredients,
-  );
+	const { textDetails, totalPrice } = getPizzaDetails(type, size, items, ingredients, selectedIngredients);
 
-  const handleClickAdd = async () => {
-    onClickAdd?.();
-    console.log({ size, type, ingredients, selectedIngredients });
-    // try {
-    //   await addPizza();
-    //   onClickAdd?.();
-    // } catch (error) {
-    //   toast.error('Произошла ошибка при добавлении в корзину');
-    //   console.error(error);
-    // }
-  };
+	const handleClickAdd = async () => {
+		onClickAdd?.();
+		console.log({ size, type, ingredients, selectedIngredients });
+		// try {
+		//   await addPizza();
+		//   onClickAdd?.();
+		// } catch (error) {
+		//   toast.error('Произошла ошибка при добавлении в корзину');
+		//   console.error(error);
+		// }
+	};
 
-  return (
-    <div className={cn(className, 'flex flex-1 ')}>
-      <ProductImage imageUrl={imageUrl} size={size} className="w-[400px]" />
+	return (
+		<div className={cn(className, "flex flex-1 ")}>
+			<ProductImage imageUrl={imageUrl} size={size} className="w-[400px]" />
 
-      <div className="w-[490px] bg-[#FCFCFC] p-7">
-        <Title text={name} size="md" className="font-extrabold mb-1" />
+			<div className="w-[490px] bg-[#FCFCFC] p-7">
+				<Title text={name} size="md" className="font-extrabold mb-1" />
 
-        <p className="text-gray-400">{textDetails}</p>
+				<p className="text-gray-400">{textDetails}</p>
 
-        <div className=" flex flex-col ga-4 mt-5">
-          <GroupVariants
-            items={availableSizes}
-            selectedValue={String(size)}
-            onClick={value => setSize(Number(value) as PizzaSize)}
-            className="mb-5"
-          />
-          <GroupVariants
-            items={pizzaTypes}
-            selectedValue={String(type)}
-            onClick={value => setType(Number(value) as PizzaType)}
-            className="mb-5"
-          />
-        </div>
+				<div className=" flex flex-col ga-4 mt-5">
+					<GroupVariants
+						items={availableSizes}
+						selectedValue={String(size)}
+						onClick={(value) => setSize(Number(value) as PizzaSize)}
+						className="mb-5"
+					/>
+					<GroupVariants
+						items={pizzaTypes}
+						selectedValue={String(type)}
+						onClick={(value) => setType(Number(value) as PizzaType)}
+						className="mb-5"
+					/>
+				</div>
 
-        <div className="bg-gray-50 p-5 rounded-md h-[420px] overflow-auto scrollbar  mb-3">
-          <IngredientsList
-            ingredients={ingredients}
-            onClickAdd={addIngredient}
-            selectedIds={selectedIngredients}
-          />
-        </div>
+				<div className="bg-gray-50 p-5 rounded-md h-[420px] overflow-auto scrollbar  mb-3">
+					<IngredientsList
+						ingredients={ingredients}
+						onClickAdd={addIngredient}
+						selectedIds={selectedIngredients}
+					/>
+				</div>
 
-        <Button
-          loading={false}
-          onClick={handleClickAdd}
-          className="h-[55px] px-10 text-base rounded-[18px] w-full"
-        >
-          Добавить в корзину за {totalPrice} ₽
-        </Button>
-      </div>
-    </div>
-  );
+				<Button
+					loading={false}
+					onClick={handleClickAdd}
+					className="h-[55px] px-10 text-base rounded-[18px] w-full"
+				>
+					Добавить в корзину за {totalPrice} ₽
+				</Button>
+			</div>
+		</div>
+	);
 };
--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,60 +1,59 @@
-import * as React from 'react';
-import { Slot } from '@radix-ui/react-slot';
-import { cva, type VariantProps } from 'class-variance-authority';
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+import * as React from "react";
 
-import { cn } from '@/lib/utils';
-import { Loader2 } from 'lucide-react';
+import { cn } from "@/lib/utils";
+import { Loader2 } from "lucide-react";
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center whitespace-nowrap rounded-md active:translate-y-[1px] text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 disabled:bg-gray-500',
-  {
-    variants: {
-      variant: {
-        default: 'bg-[#FE5F00] text-white shadow-xs hover:bg-[#e65500]',
-        destructive:
-          'bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60',
-        outline:
-          'border-[1px] border-[#FE5F00] text-[#FE5F00] bg-transparent shadow-xs hover:bg-[#FE5F00]/10',
-        secondary: 'bg-secondary text-primary hover:bg-secondary/50',
-        ghost: 'hover:bg-secondary hover:text-secondary-foreground',
-        link: 'text-primary underline-offset-4 hover:underline',
-      },
-      size: {
-        default: 'h-10 px-4 py-2',
-        sm: 'h-9 rounded-md px-3',
-        lg: 'h-11 rounded-md px-8',
-        icon: 'h-10 w-10',
-      },
-    },
-    defaultVariants: {
-      variant: 'default',
-      size: 'default',
-    },
-  },
+	"inline-flex items-center cursor-pointer justify-center whitespace-nowrap rounded-md active:translate-y-[1px] text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 disabled:bg-gray-500",
+	{
+		variants: {
+			variant: {
+				default: "bg-[#FE5F00] text-white shadow-xs hover:bg-[#e65500]",
+				destructive:
+					"bg-destructive text-white shadow-xs hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60",
+				outline: "border-[1px] border-[#FE5F00] text-[#FE5F00] bg-transparent shadow-xs hover:bg-[#FE5F00]/10",
+				secondary: "bg-secondary text-primary hover:bg-secondary/50",
+				ghost: "hover:bg-secondary hover:text-secondary-foreground",
+				link: "text-primary underline-offset-4 hover:underline",
+			},
+			size: {
+				default: "h-10 px-4 py-2",
+				sm: "h-9 rounded-md px-3",
+				lg: "h-11 rounded-md px-8",
+				icon: "h-10 w-10",
+			},
+		},
+		defaultVariants: {
+			variant: "default",
+			size: "default",
+		},
+	},
 );
 
 export interface ButtonProps
-  extends React.ButtonHTMLAttributes<HTMLButtonElement>,
-    VariantProps<typeof buttonVariants> {
-  asChild?: boolean;
-  loading?: boolean;
+	extends React.ButtonHTMLAttributes<HTMLButtonElement>,
+		VariantProps<typeof buttonVariants> {
+	asChild?: boolean;
+	loading?: boolean;
 }
 
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
-  ({ className, variant, size, asChild = false, children, disabled, loading, ...props }, ref) => {
-    const Comp = asChild ? Slot : 'button';
-    return (
-      <Comp
-        disabled={disabled || loading}
-        className={cn(buttonVariants({ variant, size, className }))}
-        ref={ref}
-        {...props}
-      >
-        {!loading ? children : <Loader2 className="w-5 h-5 animate-spin" />}
-      </Comp>
-    );
-  },
+	({ className, variant, size, asChild = false, children, disabled, loading, ...props }, ref) => {
+		const Comp = asChild ? Slot : "button";
+		return (
+			<Comp
+				disabled={disabled || loading}
+				className={cn(buttonVariants({ variant, size, className }))}
+				ref={ref}
+				{...props}
+			>
+				{!loading ? children : <Loader2 className="w-5 h-5 animate-spin" />}
+			</Comp>
+		);
+	},
 );
-Button.displayName = 'Button';
+Button.displayName = "Button";
 
 export { Button, buttonVariants };
