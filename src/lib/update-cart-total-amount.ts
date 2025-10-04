bb5a987bcaf6cad7e2c9de84d040cfbe60fbd894
romanchykov-webdev <romanchykov.webdev@gmail.com>
2025-10-04 11:33:51 +0200

    -Add commit consolelog

--- src/lib/update-cart-total-amount.ts
+++ src/lib/update-cart-total-amount.ts
@@ -2,7 +2,7 @@ import { prisma } from "../../prisma/prisma-client";
 import { calcCatItemTotalPrice } from "./calc-cart-item-total-price";
 
 export const updateCartTotalAmount = async (token: string) => {
-	console.time("FIND_USER_CART"); // Измерение времени поиска корзины
+	// console.time("FIND_USER_CART"); // Измерение времени поиска корзины
 
 	const userCart = await prisma.cart.findFirst({
 		where: {
@@ -25,19 +25,19 @@ export const updateCartTotalAmount = async (token: string) => {
 		},
 	});
 
-	console.timeEnd("FIND_USER_CART");
+	// console.timeEnd("FIND_USER_CART");
 
 	if (!userCart) return;
 
-	console.time("CALCULATE_TOTAL"); // Измерение времени расчета суммы
+	// console.time("CALCULATE_TOTAL"); // Измерение времени расчета суммы
 
 	const totalAmount = userCart?.items.reduce((acc, item) => {
 		return acc + calcCatItemTotalPrice(item);
 	}, 0);
 
-	console.timeEnd("CALCULATE_TOTAL");
+	// console.timeEnd("CALCULATE_TOTAL");
 
-	console.time("UPDATE_CART_DB"); // Измерение времени обновления в БД
+	// console.time("UPDATE_CART_DB"); // Измерение времени обновления в БД
 
 	return await prisma.cart.update({
 		where: {
